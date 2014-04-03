williamstein/bup-1

Add a VFS path_info() command and corresponding server 'path-info' command.
Provide further support for "bup get".

Signed-off-by: Rob Browning <rlb@defaultvalue.org>

lib/bup/t/tclient.py
@@ -1,16 +1,32 @@
 import sys, os, stat, time, random, subprocess, glob, tempfile
+from subprocess import check_call, check_output
 from bup import client, git
 from bup.helpers import mkdirp
 from wvtest import *
 
-bup_tmp = os.path.realpath('../../../t/tmp')
-mkdirp(bup_tmp)
+
+def ex(*cmd):
+    cmd_str = ' '.join(cmd)
+    print >> sys.stderr, cmd_str
+    check_call(cmd)
+
+
+def exo(*cmd):
+    cmd_str = ' '.join(cmd)
+    print >> sys.stderr, cmd_str
+    return check_output(cmd)
+
 
 def randbytes(sz):
     s = ''
     for i in xrange(sz):
         s += chr(random.randrange(0,256))
     return s
+
+top_dir = os.path.realpath('../../..')
+bup_exe = top_dir + '/bup'
+bup_tmp = os.path.realpath(top_dir + '/t/tmp')
+mkdirp(bup_tmp)
 
 s1 = randbytes(10000)
 s2 = randbytes(10000)
@@ -162,3 +178,93 @@ def test_remote_parsing():
         WVFAIL()
     except client.ClientError:
         WVPASS()
+
+
+@wvtest
+def test_path_info():
+    initial_failures = wvfailure_count()
+    tmpdir = tempfile.mkdtemp(dir=bup_tmp, prefix='bup-tclient-')
+    os.environ['BUP_MAIN_EXE'] = '../../../bup'
+    os.environ['BUP_DIR'] = bupdir = tmpdir
+    src = tmpdir + '/src'
+    mkdirp(src)
+    with open(src + '/1', 'w+') as f:
+        print f, 'something'
+    with open(src + '/2', 'w+') as f:
+        print f, 'something else'
+    os.mkdir(src + '/dir')
+    git.init_repo(bupdir)
+    c = client.Client(bupdir, create=True)
+
+    info = c.path_info(['/'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    name, id, type = info[0]
+    WVPASSEQ(type, 'root')
+
+    info = c.path_info(['/not-there/'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0] is None)
+
+    data = exo(bup_exe, 'random', '128k' )
+    with open(src + '/chunky', 'wb+') as f:
+        f.write(data)
+    ex(bup_exe, 'index', '-vv', src)
+    ex(bup_exe, 'save', '-n', 'src', '--strip', src)
+    ex(bup_exe, 'tag', 'src-latest-tag', 'src')
+    src_hash = exo('git', '--git-dir', bupdir,
+                   'rev-parse', 'src').strip().split('\n')
+    assert(len(src_hash) == 1)
+    src_hash = src_hash[0].decode('hex')
+    tree_hash = exo('git', '--git-dir', bupdir,
+                   'rev-parse', 'src:dir').strip().split('\n')[0].decode('hex')
+    file_hash = exo('git', '--git-dir', bupdir,
+                   'rev-parse', 'src:1').strip().split('\n')[0].decode('hex')
+    chunky_hash = exo('git', '--git-dir', bupdir,
+                      'rev-parse', 'src:chunky.bup').strip().split('\n')[0].decode('hex')
+    info = c.path_info(['/src'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src', src_hash, 'branch'])
+
+    info = c.path_info(['/src/latest'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest', src_hash, 'save'])
+
+    info = c.path_info(['/src/latest/dir'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest/dir', tree_hash, 'dir'])
+
+    info = c.path_info(['/src/latest/1'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest/1', file_hash, 'file'])
+
+    info = c.path_info(['/src/latest/chunky'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest/chunky', chunky_hash, 'chunked-file'])
+
+    info = c.path_info(['/.tag/src-latest-tag'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/.tag/src-latest-tag', src_hash, 'commit'])
+
+    info = c.path_info(['.tag////src-latest-tag'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/.tag/src-latest-tag', src_hash, 'commit'])
+
+    if wvfailure_count() == initial_failures:
+        subprocess.call(['rm', '-rf', tmpdir])

tjanez/bup

lib/bup/t/tclient.py
@@ -1,16 +1,32 @@
 import sys, os, stat, time, random, subprocess, glob, tempfile
+from subprocess import check_call
 from bup import client, git
-from bup.helpers import mkdirp
+from bup.helpers import mkdirp, readpipe
 from wvtest import *
 
-bup_tmp = os.path.realpath('../../../t/tmp')
-mkdirp(bup_tmp)
+
+def ex(*cmd):
+    cmd_str = ' '.join(cmd)
+    print >> sys.stderr, cmd_str
+    check_call(cmd)
+
+
+def exo(*cmd):
+    cmd_str = ' '.join(cmd)
+    print >> sys.stderr, cmd_str
+    return readpipe(cmd)
+
 
 def randbytes(sz):
     s = ''
     for i in xrange(sz):
         s += chr(random.randrange(0,256))
     return s
+
+top_dir = os.path.realpath('../../..')
+bup_exe = top_dir + '/bup'
+bup_tmp = os.path.realpath(top_dir + '/t/tmp')
+mkdirp(bup_tmp)
 
 s1 = randbytes(10000)
 s2 = randbytes(10000)
@@ -162,3 +178,93 @@ def test_remote_parsing():
         WVFAIL()
     except client.ClientError:
         WVPASS()
+
+
+@wvtest
+def test_path_info():
+    initial_failures = wvfailure_count()
+    tmpdir = tempfile.mkdtemp(dir=bup_tmp, prefix='bup-tclient-')
+    os.environ['BUP_MAIN_EXE'] = '../../../bup'
+    os.environ['BUP_DIR'] = bupdir = tmpdir
+    src = tmpdir + '/src'
+    mkdirp(src)
+    with open(src + '/1', 'w+') as f:
+        print f, 'something'
+    with open(src + '/2', 'w+') as f:
+        print f, 'something else'
+    os.mkdir(src + '/dir')
+    git.init_repo(bupdir)
+    c = client.Client(bupdir, create=True)
+
+    info = c.path_info(['/'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    name, id, type = info[0]
+    WVPASSEQ(type, 'root')
+
+    info = c.path_info(['/not-there/'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0] is None)
+
+    data = exo(bup_exe, 'random', '128k')
+    with open(src + '/chunky', 'wb+') as f:
+        f.write(data)
+    ex(bup_exe, 'index', '-vv', src)
+    ex(bup_exe, 'save', '-n', 'src', '--strip', src)
+    ex(bup_exe, 'tag', 'src-latest-tag', 'src')
+    src_hash = exo('git', '--git-dir', bupdir,
+                   'rev-parse', 'src').strip().split('\n')
+    assert(len(src_hash) == 1)
+    src_hash = src_hash[0].decode('hex')
+    tree_hash = exo('git', '--git-dir', bupdir,
+                   'rev-parse', 'src:dir').strip().split('\n')[0].decode('hex')
+    file_hash = exo('git', '--git-dir', bupdir,
+                   'rev-parse', 'src:1').strip().split('\n')[0].decode('hex')
+    chunky_hash = exo('git', '--git-dir', bupdir,
+                      'rev-parse', 'src:chunky.bup').strip().split('\n')[0].decode('hex')
+    info = c.path_info(['/src'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src', src_hash, 'branch'])
+
+    info = c.path_info(['/src/latest'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest', src_hash, 'save'])
+
+    info = c.path_info(['/src/latest/dir'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest/dir', tree_hash, 'dir'])
+
+    info = c.path_info(['/src/latest/1'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest/1', file_hash, 'file'])
+
+    info = c.path_info(['/src/latest/chunky'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/src/latest/chunky', chunky_hash, 'chunked-file'])
+
+    info = c.path_info(['/.tag/src-latest-tag'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/.tag/src-latest-tag', src_hash, 'commit'])
+
+    info = c.path_info(['.tag////src-latest-tag'])
+    WVPASS(info)
+    WVPASS(len(info) == 1)
+    WVPASS(info[0])
+    WVPASSEQ(info[0], ['/.tag/src-latest-tag', src_hash, 'commit'])
+
+    if wvfailure_count() == initial_failures:
+        subprocess.call(['rm', '-rf', tmpdir])